655e5268b7e868ccb7b8f3a8369d8de209094152
Levi Rizki Saputra <levirs565@gmail.com>
2019-04-26 19:09:47 +0700

    create Soal component

---
+++ src/components/Soal.vue
@@ -0,0 +1,27 @@
+<template>
+  <form class="soal">
+    <p v-text="soal"></p>
+    <template v-for="(entry, index) in pilihan">
+      <input type="radio" :id="'pilihan' + index" name="pilihan" :key="index + 'i'" :value="index">
+      <label :for="'pilihan' + index" :key="index + 'l'" v-text="entry"></label>
+      <br :key="index + 'br'">
+    </template>
+    <input type="button" :value="teksSubmit">
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+    soal: String,
+    pilihan: Array,
+    teksSubmit: {
+      type: String,
+      default: "Check Jawaban"
+    }
+  }
+};
+</script>
+
+<style>
+</style>
