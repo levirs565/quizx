d8c2458941c61579fb72e025fea3bd1834915a8f
Levi Rizki Saputra <levirs565@gmail.com>
2019-04-27 07:39:37 +0700

    menambahkan client database

---
+++ src/database.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/soalku");
+
+var soalScheme = new mongoose.Schema({
+  _id: Number,
+  soal: String,
+  pilihan: Array
+}, {
+    collection: "soal"
+  }
+)
+
+var Soal = mongoose.model('Soal', soalScheme);
+
+function getSoal(id) {
+  return Soal.findById(id)
+}
+exports.getSoal = getSoal
