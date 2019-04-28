378d61312c68f71438f39a6aa1d97bd5ba31a70e
Levi Rizki Saputra <levirs565@gmail.com>
2019-04-28 09:49:21 +0700

    menambahkan managemen session

---
+++ src/session.js
@@ -0,0 +1,13 @@
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
+
+const savedSession = session({
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+  }),
+  secret: 'wedus nabrak witk kambil neng sor mahku',
+  resave: true,
+  saveUninitialized: false,
+});
+exports.savedSession = savedSession;
