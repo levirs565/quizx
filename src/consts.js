ddd1c947bebf8a82e48c4591640c2c0eacff5066
Levi Rizki Saputra <levirs565@gmail.com>
2019-05-01 16:11:46 +0700

    menamabhkan msg on permainan

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,6 @@
 exports.MSG_SOAL_NF = 'Soal tidak ada.';
 exports.MSG_SERVER_READY = 'Server telah siap';
 exports.MSG_KESALAHAN_ANEH = 'Kesalahan aneh';
+exports.MSG_ON_PERMAINAN = 'Anda dalam permainan';
 
 exports.DB_SOALKU = 'mongodb://127.0.0.1:27017/soalku';
