c8fb01f9650220807984aa431db44249ee74f063
Levi Rizki Saputra <levirs565@gmail.com>
2019-05-01 16:24:32 +0700

    mengimplementasikan api permainan start

---
+++ src/api/permainan.js
@@ -0,0 +1,26 @@
+const app = require('../app');
+const utils = require('../helper/utils');
+const consts = require('../consts');
+const helper = require('../helper/permainan');
+const soal = require('../helper/soal');
+
+app.post('/api/permainan/start', (req, res) => {
+  utils.handleRequest(
+    new Promise((resolve, reject) => {
+      if (helper.isOnPermainan(req)) {
+        reject(Error(consts.MSG_ON_PERMAINAN));
+      }
+
+      helper.setOnPermainan(req, true);
+      helper.setJawabanCollection(req, []);
+      soal.createRandomSoalCollection(40).then(val => {
+        helper.setSoalCollection(req, val);
+        resolve(true);
+      });
+    }),
+    () => ({}),
+    consts.MSG_KESALAHAN_ANEH,
+    req,
+    res
+  );
+});
