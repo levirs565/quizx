7f79382d35c01d7b4753e9aa16c44071ab99576a
Levi Rizki Saputra <levirs565@gmail.com>
2019-05-01 16:23:51 +0700

    mentrace kesalahan pada handleRequest

--- src/helper/utils.js
+++ src/helper/utils.js
@@ -14,6 +14,7 @@ function handleRequest(promise, funcRes, nullMsg, req, res) {
       });
     },
     err => {
+      console.log(err);
       res.json({
         err: true,
         msg: err.toString(),
