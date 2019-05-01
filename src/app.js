7eef89fc9ccac70b04047c275a850abf17a4fc78
Levi Rizki Saputra <levirs565@gmail.com>
2019-05-01 16:22:31 +0700

    menambahkan session ke app

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const session = require('./helper/session');
 
 const app = express();
 
 app.use(express.json());
+app.use(session.savedSession);
 
 app.get('/api/test', (req, res) => {
   res.json({
