37b5297098cd34df2937a69be501c6a281015e04
Levi Rizki Saputra <levirs565@gmail.com>
2019-05-03 18:55:01 +0700

    menambahkan msg n finished

--- src/consts.js
+++ src/consts.js
@@ -3,5 +3,6 @@ exports.MSG_SERVER_READY = 'Server telah siap';
 exports.MSG_KESALAHAN_ANEH = 'Kesalahan aneh';
 exports.MSG_ON_PERMAINAN = 'Anda dalam permainan';
 exports.MSG_N_ON_PERMAINAN = 'Anda tidak dalam permainan';
+exports.MSG_N_FINISHED = 'Anda belum menyelesaikan permainan';
 
 exports.DB_SOALKU = 'mongodb://127.0.0.1:27017/soalku';
