1b8b054137c65d7275f2d627b048579c3cecfe9c
Levi Rizki Saputra <levirs565@gmail.com>
2019-05-04 15:49:05 +0700

    menambahkan view home

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="app">
+    <button>Mulai</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.app {
+  text-align: center;
+}
+</style>
