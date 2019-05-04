fdb8ea55bca2cc1720e8ed089410e623df54aed6
Levi Rizki Saputra <levirs565@gmail.com>
2019-05-04 15:46:52 +0700

    menambahkan cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const session = require('./helper/session');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(session.savedSession);
 
