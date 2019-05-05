9dbf798a631cd1750ce10808cf4ea230fee0d2aa
Levi Rizki Saputra <levirs565@gmail.com>
2019-05-05 14:47:15 +0700

    mengupdate api

--- src/api.js
+++ src/api.js
@@ -1,11 +1,27 @@
-import axios from 'axios'
+import axios from 'axios';
 
 export function getSoal(id) {
-  return axios.get(`http://localhost:3000/api/soal/${id}`)
+  return axios.get(`http://localhost:3000/api/soal/${id}`, {
+    withCredentials: true
+  });
 }
 
 export function checkJawaban(id, jawaban) {
   return axios.post(`http://localhost:3000/api/jawab/${id}`, {
     jawaban: jawaban
+  }, {
+    withCredentials: true
+  });
+}
+
+export function startPermainan() {
+  return axios.post('http://localhost:3000/api/permainan/start', {}, {
+    withCredentials: true
+  });
+}
+
+export function stopPermainan() {
+  return axios.post('http://localhost:3000/api/permainan/stop', {}, {
+    withCredentials: true
   });
 }
