0f41b3dafc576d3ec545cf1d5ff76c47ee7598e7
Levi Rizki Saputra <levirs565@gmail.com>
2019-05-05 15:31:27 +0700

    menambahkan soal

--- src/views/Permainan.vue
+++ src/views/Permainan.vue
@@ -5,22 +5,35 @@
     <br/>
     <ul v-if="onPermainan" style="list-type: none;">
       <li style="display: inline;" v-for="index in Array(soalCount).keys()" :key="index">
-        <button class="small" @click="jumpSoal" data-soal-id="10">{{ index + 1 }}</button>
+        <button class="small" @click="jumpSoal" :data-soal-id="index">{{ index + 1 }}</button>
       </li>
     </ul>
+    <soal
+      v-if="currentSoal != undefined"
+      :soal="currentSoal.soal"
+      :pilihan="currentSoal.pilihan"
+      :soalId="currentSoal.id"
+      teksSubmit="Kirim Jawaban"
+      class="soal"
+    ></soal>
   </div>
 </template>
 
 <script>
-import { startPermainan, stopPermainan } from '../api.js'
+import { startPermainan, stopPermainan, getSoalPermainan } from "../api.js";
+import Soal from "../components/Soal.vue";
 
 export default {
+  components: {
+    Soal
+  },
   data() {
     return {
       onPermainan: false,
       soalCount: 40,
       lastSelectedElement: undefined,
-    }
+      currentSoal: undefined
+    };
   },
   methods: {
     start() {
@@ -45,25 +58,47 @@ export default {
         }
       });
     },
+    goToSoal(id) {
+      getSoalPermainan(id).then(res => {
+        let data = res.data;
+
+        if (data.err) {
+          console.log(data.msg);
+        } else {
+          this.currentSoal = data.soal;
+        }
+      });
+    },
     jumpSoal(e) {
-      let target = e.target; 
+      let target = e.target;
 
       if (target == this.lastSelectedElement) {
         return;
       }
 
-      target.classList.add('selected');
+      target.classList.add("selected");
       if (this.lastSelectedElement !== undefined) {
-        this.lastSelectedElement.classList.remove('selected');
+        this.lastSelectedElement.classList.remove("selected");
       }
       this.lastSelectedElement = target;
+
+      this.goToSoal(Number(target.dataset.soalId));
     }
   }
-}
+};
 </script>
 
 <style>
 .permainan {
   text-align: center;
+}
+
+.soal {
+  text-align: left; 
+  padding: 2.5% 15%;
+}
+
+.soal > input[type=button] {
+  float: right;
 }
 </style>
