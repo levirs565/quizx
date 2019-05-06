75d3b000fc390976063df4619ba5c3ab58496582
Levi Rizki Saputra <levirs565@gmail.com>
2019-05-06 10:48:04 +0700

    membuat komponen jumper

---
+++ src/components/Jumper.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="jumper">
+    <button
+      v-for="index in total"
+      :key="index"
+      @click="jumpClick(index)"
+      :class="{
+        'small': true,
+        'selected': current == index
+      }"
+    >{{ index }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["total"],
+  data() {
+    return {
+      current: 1
+    };
+  },
+  methods: {
+    jumpClick(id) {
+      this.current = id;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
