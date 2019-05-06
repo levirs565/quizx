d5badf04dfae0312aba8ba91c08f34e61fc2426e
Levi Rizki Saputra <levirs565@gmail.com>
2019-05-06 09:45:07 +0700

    menambahkan komponen permainan resulrs

---
+++ src/components/PermainanResult.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="permainan-result">
+    <p>Hasil Permainan</p>
+    <p>Benar: {{ results.benar }}</p>
+    <p>Salah: {{ results.salah }}</p>
+    <p>Tidak di jawab: {{ results.takDiJawab }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['results']
+}
+</script>
+
+<style>
+
+</style>
