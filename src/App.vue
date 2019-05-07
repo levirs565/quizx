2dc2f718a5e32a53a7965c933101c8ef7ff7b594
Levi Rizki Saputra <levirs565@gmail.com>
2019-05-07 17:31:06 +0700

    menghapus semua soal pada App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,18 @@
 <template>
   <div id="app">
-    <!-- <soal :soal="soal.soal" :pilihan="soal.pilihan" :soalId="soal._id" @submit="soalSubmit"></soal>
-    <br>
-    <button @click="soalLain(-1)" style="float: left;" :disabled="soal._id <= 0">Soal Sebelumnya</button>
-    <button @click="soalLain(1)" style="float: right;">Soal Selanjutnya</button>-->
-    <router-view class="router-view"></router-view>
+    <router-view class="router-view container"></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-import Soal from "./components/Soal.vue";
-import { getSoal, checkJawaban } from "./api.js";
-
 export default {
   name: "app",
-  components: {
-    HelloWorld,
-    Soal
-  },
-  data() {
-    return {
-      soal: {
-        _id: 0,
-        soal: "",
-        pilihan: []
-      }
-    };
-  },
-  methods: {
-    gantiSoal(id) {
-      getSoal(id).then(
-        val => {
-          this.$set(this, "soal", Object(val.data.soal));
-        },
-        err => {
-          console.log(err);
-        }
-      );
-    },
-    soalLain(inc) {
-      let id = this.soal._id + inc;
-      if (id >= 0) {
-        this.gantiSoal(id);
-      }
-    },
-    soalSubmit(th) {
-      checkJawaban(th.soalId, th.pilihanTerpilih).then(val => {
-        let benar = val.data.benar;
-        alert(`Jawaban anda ${benar ? "Benar" : "Salah"}`);
-      });
-    }
-  },
-  mounted() {
-    this.gantiSoal(0);
-  }
 };
 </script>
 
 <style>
+@import '../node_modules/bulma/css/bulma.css';
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -68,7 +22,7 @@ export default {
   padding: 0 0.5rem;
 }
 
-button, input[type=button] {
+/* button, input[type=button] {
   background-color: darkgreen;
   border: none;
   padding: 10px 50px;
@@ -93,5 +47,5 @@ button.small {
 
 button.selected {
   background-color: olivedrab;
-}
+} */
 </style>
