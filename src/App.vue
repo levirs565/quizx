71627fbe307f4f5988b3d2ca49fad0768c96ca30
Levi Rizki Saputra <levirs565@gmail.com>
2019-05-08 13:14:53 +0700

    menamabahkan navbar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,62 @@
 <template>
   <div id="app">
-    <router-view class="router-view container"></router-view>
+    <nav class="navbar is-primary">
+      <div class="navbar-brand">
+        <a
+          class="navbar-burger"
+          :class="menuClass"
+          @click="burgerActive = !burgerActive"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div class="navbar-menu" :class="menuClass">
+        <div class="navbar-start">
+          <a class="navbar-item" href="/#/">Home</a>
+          <a class="navbar-item" href="/#/permainan">Permainan</a>
+        </div>
+      </div>
+    </nav>
+    <router-view class="router-view container is-spaced"></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: "app"
+  name: "app",
+  data() {
+    return {
+      burgerActive: false
+    };
+  },
+  computed: {
+    menuClass() {
+      return {
+        "is-active": this.burgerActive
+      };
+    }
+  }
 };
 </script>
 
 <style>
 @import "../node_modules/bulma/css/bulma.css";
 
-#app {
+.router-view {
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+}
+
+/* #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   margin-top: 60px;
   padding: 0 0.5rem;
-}
+} */
 
 /* button, input[type=button] {
   background-color: darkgreen;
