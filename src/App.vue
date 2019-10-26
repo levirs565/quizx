e6b921741bd6a875b9febc8d12c4d2a28c6e98fb
Levi Rizki Saputra <levirs565@gmail.com>
2019-10-26 19:02:53 +0700

    reimplement navigation

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,7 @@
 <template>
   <div id="app" class="flex flex-col h-full absolute w-full">
     <notification-container ref="nof"></notification-container>
-    <nav ref="nav">
-      <div class="logo"></div>
-      <div class="burger">
-        <button @click="burgerActive = !burgerActive">
-          <font-awesome icon="bars"></font-awesome>
-        </button>
-      </div>
-      <div class="items" :class="{
-        'active': burgerActive
-      }">
-        <a href="/#/" :class="{
-          selected: this.$route.path == '/'
-        }">Home</a>
-        <a
-          :class="{
-            selected: this.$route.path.startsWith('/permainan')
-          }"
-          href="/#/permainan"
-        >Permainan</a>
-      </div>
-    </nav>
+    <navigation></navigation>
     <div class="flex flex-grow">
       <router-view></router-view>
     </div>
@@ -30,30 +10,14 @@
 
 <script>
 import NotificationContainer from "./components/NotificationContainer.vue";
+import Navigation from "./components/Navigation";
 
 export default {
   components: {
-    NotificationContainer
+    NotificationContainer,
+    Navigation
   },
   name: "app",
-  data() {
-    return {
-      burgerActive: false,
-      baseNavClass:
-        "w-full lg:w-auto lg:mr-2 mt-2 lg:mt-0 py-1 px-3 block lg:inline-flex rounded",
-      normalNavClass:
-        "text-white hover:text-white hover:bg-green-600 border border-green-500",
-      selectedNavClass:
-        "bg-white text-green-500 hover:text-green-500 border border-white"
-    };
-  },
-  computed: {
-    menuClass() {
-      return {
-        "is-active": this.burgerActive
-      };
-    }
-  },
   mounted() {
     this.notification.container = this.$refs.nof;
   }
@@ -61,66 +25,4 @@ export default {
 </script>
 
 <style>
-/* @import "../node_modules/bulma/css/bulma.css"; */
-
-/* .router-view {
-  padding: 0.75rem;
-}
-
-@media screen and (min-width: 769px), print {
-  .router-view {
-  }
-}
-
-.linebreak {
-  width: 100%;
-  height: 0;
-  display: block;
-}
-
-.center-margin {
-  margin-left: auto !important;
-  margin-right: auto !important;
-}
-
-.vcenter-margin {
-  margin-top: auto !important;
-  margin-bottom: auto !important;
-} */
-
-/* #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 60px;
-  padding: 0 0.5rem;
-} */
-
-/* button, input[type=button] {
-  background-color: darkgreen;
-  border: none;
-  padding: 10px 50px;
-  font-size: 16px;
-  color: white;
-}
-button:hover, input[type=button]:hover {
-  background-color: green;
-}
-
-button:active, input[type=button]:active {
-  background-color: lightgreen;
-}
-
-button:disabled, input[type=button]:disabled {
-  background-color: olive;
-}
-
-button.small {
-  padding: 10px 20px;
-}
-
-button.selected {
-  background-color: olivedrab;
-} */
 </style>
