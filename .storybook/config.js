59c2763c690156edf006dd98053dd713b7ca368e
Levi Rizki Saputra <levirs565@gmail.com>
2019-10-26 18:29:08 +0700

    add fontsawesome to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,17 @@
 import { configure } from '@storybook/vue';
+import Vue from 'vue';
 import '../src/style.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faCheck,
+  faTimes,
+  faExclamation,
+  faBars
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faCheck, faTimes, faExclamation, faBars);
+
+Vue.component('font-awesome', FontAwesomeIcon);
 
 configure(require.context('../src', true, /\.stories\.js$/), module);
