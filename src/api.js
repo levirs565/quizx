3f1aa18fd59a9c79b6788806f2f3a2099295703a
Levi Rizki Saputra <levirs565@gmail.com>
2019-11-20 09:29:50 +0700

    reimplement soal api

--- src/api.js
+++ src/api.js
@@ -15,27 +15,22 @@ const instance = axios.create({
   withCredentials: true
 });
 
-class SoalImpl {
+export const Soal = {
   getPaketList() {
     return instance.get('/soal').then(throwError);
-  }
-
+  },
   getPaket(id) {
     return instance.get(`/soal/${id}`).then(throwError);
-  }
-
+  },
   getSoal(colId, soalId) {
     return instance.get(`/soal/${colId}/${soalId}`).then(throwError);
-  }
-
+  },
   postJawaban(colId, soalId, jawaban) {
     return instance
       .post(`/soal/${colId}/${soalId}`, { jawaban })
       .then(throwError);
   }
-}
-
-export const Soal = new SoalImpl();
+};
 
 export const User = {
   signup(id, name, password) {
