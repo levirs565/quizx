60cce205937a2d5c691f3dbf3fc99a69bbe839ca
Levi Rizki Saputra <levirs565@gmail.com>
2019-11-22 15:19:39 +0700

    reimplement navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,28 +1,36 @@
 <template>
   <nav>
-    <div class="logo"></div>
-    <div class="burger">
+    <div class="nav-logo">
+      <span>soalku</span>
+    </div>
+    <div class="nav-burger">
       <button @click="burgerActive = !burgerActive">
         <font-awesome icon="bars"></font-awesome>
       </button>
     </div>
-    <div class="items" :class="{
+    <div class="nav-items" :class="{
         'active': burgerActive
       }">
-      <navigation-link to="/">Home</navigation-link>
-      <navigation-link to="/soal">Paket Soal</navigation-link>
-      <navigation-link to="/permainan/config">Permainan</navigation-link>
-      <navigation-link to="/login" v-show="!username">Login</navigation-link>
-    </div>
+      <div class="start">
+        <navigation-link to="/">Home</navigation-link>
+        <navigation-link to="/soal">Paket Soal</navigation-link>
+        <navigation-link to="/permainan/config">Permainan</navigation-link>
+      </div>
 
-    <div class="user" v-if="username" @click="toggleUserBox">
-      <font-awesome icon="user" size="lg"></font-awesome>
+      <div class="end" v-if="!username">
+        <router-link class="button mr-4" to="/login">Login</router-link>
+        <router-link class="button" to="/register">Register</router-link>
+      </div>
 
-      <div ref="userbox" class="box" v-show="showUserBox" v-click-outside="hideUserBox">
-        <font-awesome icon="user" size="6x" class="w-full"></font-awesome>
-        <p v-text="username.name" class="font-semibold text-center w-full mb-4"></p>
+      <div class="end user" v-else @click="toggleUserBox">
+        <font-awesome icon="user" size="lg"></font-awesome>
 
-        <button class="button w-full" @click="userLogout">Logout</button>
+        <div ref="userbox" class="box" v-show="showUserBox" v-click-outside="hideUserBox">
+          <font-awesome icon="user" size="6x" class="w-full"></font-awesome>
+          <p v-text="username.name" class="font-semibold text-center w-full mb-4"></p>
+
+          <button class="button w-full" @click="userLogout">Logout</button>
+        </div>
       </div>
     </div>
   </nav>
