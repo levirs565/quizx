60eecb6592f6ceaa9dee5f730153d5163e4dc665
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-07-14 21:53:05 +0700

    feat(client): add edit mode in quiz admin

--- client/src/admin/soal/components/QuestionAdmin.vue
+++ client/src/admin/soal/components/QuestionAdmin.vue
@@ -1,13 +1,11 @@
 <template>
-  <div
-    class="box my-4"
-    >
-    <div class="overflow-auto">
+  <div class="box my-4">
+    <div class="overflow-auto mb-4">
       <span>Soal ke {{question.id + 1}}</span>
       <button class="button danger ml-4 float-right" v-show="question">
         Hapus
       </button>
-      <button class="button primary float-right" @click="isEditMode = true" v-show="!isEditMode">
+      <button class="button primary float-right" @click="toEditMode" v-show="!isEditMode">
         Edit
       </button>
       <button class="button primary float-right" @click="saveQuestion" v-show="isEditMode">
@@ -16,11 +14,44 @@
     </div>
 
     <question
+      v-if="!isEditMode"
       :question="question"
       :initialAnswer="question.jawaban"
       :radioEnabled="false"
     >
     </question>
+    <form v-else class="overflow-auto edit-view">
+      <textarea
+        class="input w-full"
+        v-model="question.soal"
+        @input="expandInput"
+        tabindex="1"
+      />
+      <div
+        v-for="(entry, index) in question.pilihan"
+        :key="index"
+        class="flex flex-row items-center pilihan"
+      >
+        <input
+          type="radio"
+          name="pilihan"
+          :value="index"
+          v-model="question.jawaban"
+        />
+        <textarea
+          v-text="entry"
+          @change="setChoiceText(index, $event.target.value)"
+          @input="expandInput"
+          class="input flex-grow"
+          :tabindex="index + 2"
+        />
+        <button class="button danger" @click="deleteChoice(index)">
+          <font-awesome icon="trash"/>
+        </button>
+      </div>
+
+      <button class="button primary float-right" @click="newChoice">Pilihan Baru</button>
+    </form>
   </div>
 </template>
 
@@ -42,10 +73,42 @@ export default {
   methods: {
     saveQuestion() {
       this.isEditMode = false
-    }
+    },
+    toEditMode() {
+      this.isEditMode = true
+      this.refreshTextArea()
+    },
+    expandInput(event) {
+      const el = event.target;
+      el.style.height = "3rem";
+      const height = el.offsetHeight - el.clientHeight + el.scrollHeight;
+      el.style.height = height + "px";
+    },
+    refreshTextArea() {
+      this.$nextTick(() => {
+        const arr = this.$el.getElementsByTagName("textarea");
+        for (let i = 0; i < arr.length; i++) {
+          const target = arr[i];
+
+          this.expandInput({ target });
+        }
+      });
+    },
+    setChoiceText(index, val) {
+      this.$set(this.question.pilihan, index, val);
+    },
+    deleteChoice(index) {
+      this.$delete(this.question.pilihan, index);
+    },
+    newChoice() {
+      this.question.pilihan.push("");
+    },
   }
 };
 </script>
 
-<style>
+<style scoped>
+.edit-view > * {
+  @apply mb-2;
+}
 </style>
