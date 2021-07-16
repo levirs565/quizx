e5de2c33d730822537b4e687e4229511c13e26ce
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-07-16 11:08:34 +0700

    refractor(client): content/permainan folder to game

--- client/src/content/router.js
+++ client/src/content/router.js
@@ -1,12 +1,12 @@
 import ContentContainer from './Content.vue';
 import SiteRouter from './site/router';
 import QuizRouter from './quiz/router';
-import PermainanRouter from './permainan/router';
+import GameRouter from './game/router';
 
 export default [
   {
     path: '/',
     component: ContentContainer,
-    children: [...SiteRouter, ...QuizRouter, ...PermainanRouter]
+    children: [...SiteRouter, ...QuizRouter, ...GameRouter]
   }
 ];
