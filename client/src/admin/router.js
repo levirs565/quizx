c006f7d02b0ff6673a30408fccb40d30edc30d8c
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-07-16 10:39:59 +0700

    refractor(client): rename admin/soal to quiz

--- client/src/admin/router.js
+++ client/src/admin/router.js
@@ -1,11 +1,11 @@
 import AdminContainer from './Admin.vue';
 import SiteRouter from './site/router';
-import SoalRouter from './soal/router';
+import QuizRouter from './quiz/router';
 
 export default [
   {
     path: '/admin',
     component: AdminContainer,
-    children: [...SiteRouter, ...SoalRouter]
+    children: [...SiteRouter, ...QuizRouter]
   }
 ];
