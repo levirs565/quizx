88dc57db2b1cd46922535b6eca0a155d94a74ef5
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-08-05 21:14:13 +0700

    feat(client): redesign button

--- client/src/core/styles/button.stories.js
+++ client/src/core/styles/button.stories.js
@@ -1,9 +1,13 @@
 export default { title: 'Button' };
 
-export const standar = () => '<button class="button">Standar</button>';
+export const button = () => '<button class="button">Button</button>';
 
 export const primary = () => '<button class="button primary">Primary</button>';
 
 export const danger = () => '<button class="button danger">Danger</button>';
 
-export const info = () => '<button class="button info">Info</button>';
+export const outlined = () => '<button class="button outlined">Outlined</button>';
+
+export const icon = () => '<button class="button-icon"><span class="material-icons">home</span></button>'
+
+export const fab = () => '<button class="fab"><span class="material-icons">add</span></button>'
