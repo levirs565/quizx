92f6fd5e33b8e2bfbd8f74473d0b3b2c37ac7089
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-08-07 08:47:16 +0700

    refractor(client): font button as tailwind utility

--- client/tailwind.js
+++ client/tailwind.js
@@ -1,23 +1,41 @@
-const colors = require('tailwindcss/colors');
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   important: true,
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        button: [
+          "0.875rem",
+          {
+            letterSpacing: "1.25px",
+          },
+        ],
+        body1: [
+          "1rem",
+          {
+            letterSpacing: "0.5px",
+          },
+        ],
+      },
+    },
     container: {
-      padding: '1rem',
-      center: true
+      padding: "1rem",
+      center: true,
     },
     colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
+      transparent: "transparent",
+      current: "currentColor",
       black: colors.black,
       white: colors.white,
       gray: colors.gray,
       green: colors.green,
       red: colors.red,
-    }
+    },
+    fontFamily: {
+      roboto: ["Roboto"],
+    },
   },
   variants: {},
-  plugins: []
+  plugins: [],
 };
