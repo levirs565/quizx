596a14e340efcef1707d30128995c6d798498e0c
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-08-14 20:58:21 +0700

    feat(client): redesign my quiz page

--- client/src/content/quiz/views/QuizList.vue
+++ client/src/content/quiz/views/QuizList.vue
@@ -1,39 +1,58 @@
 <template>
-  <div class="box container">
-    <h1 class="title">Koleksi Soal</h1>
-    <hr class="hr" />
+  <div class="container p-4">
+    <h1 class="fonts-roboto text-headline5 mb-4">My Quiz</h1>
+
     <ul>
       <router-link
+        :to="`/quiz/${quiz.id}`"
         v-for="quiz in quizList"
         :key="quiz.id"
         tag="li"
-        :to="`/quiz/${quiz.id}`"
-        class="list-item"
+        v-slot="{ navigate }"
       >
-        <p class="text" v-text="quiz.title"></p>
-        <p class="subtext">{{ quiz.soalCount }} Soal</p>
+        <quiz-summary-card :quiz="quiz" @click="navigate" />
       </router-link>
     </ul>
+
+    <c-fab class="absolute right-4 bottom-4" @click="showCreateQuiz">
+      <c-icon>add</c-icon>
+    </c-fab>
   </div>
 </template>
 
 <script>
 import { Quiz } from "@/api.js";
+import QuizSummaryCard from "@/content/components/QuizSummaryCard.vue";
+import CFab from "@/components/CFab.vue";
+import ModalNewQuiz from "../components/ModalNewQuiz.vue";
+import showModal from "@/content/modal/bus";
 
 export default {
+  components: {
+    QuizSummaryCard,
+    CFab,
+  },
   data() {
     return {
-      quizList: []
+      quizList: [],
     };
   },
-
+  methods: {
+    showCreateQuiz() {
+      showModal(ModalNewQuiz, {}, this.createQuiz);
+    },
+    async createQuiz(title) {
+      const quiz = await Quiz.createQuiz({ title });
+      this.$router.push(`/quiz/${quiz.id}`)
+    },
+  },
   mounted() {
-    Quiz.getQuizList().then(val => {
+    // Only show current user quiz
+    Quiz.getQuizList().then((val) => {
       this.quizList = val.list;
     });
-  }
+  },
 };
 </script>
 
-<style>
-</style>
+<style></style>
