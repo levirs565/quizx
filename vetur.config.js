45f23e5d33000ed4e0f546175ad01d8411636e72
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-08-14 18:36:11 +0700

    chore: add vetur config

---
+++ vetur.config.js
@@ -0,0 +1,12 @@
+/** @type {import('vls').VeturConfig} */
+module.exports = {
+  // **optional** default: `{}`
+  // override vscode settings
+  // Notice: It only affects the settings used by Vetur.
+  settings: {
+    "vetur.useWorkspaceDependencies": true,
+  },
+  // **optional** default: `[{ root: './' }]`
+  // support monorepos
+  projects: ["./client"],
+};
