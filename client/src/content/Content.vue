e5fa840fd3721be3020f35adee263116f4cfda98
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-08-14 19:33:42 +0700

    feat(client): add simple drawer

--- client/src/content/Content.vue
+++ client/src/content/Content.vue
@@ -1,19 +1,40 @@
 <template>
   <div class="flex flex-col">
     <modal></modal>
-    <app-bar></app-bar>
-    <router-view></router-view>
+    <app-bar class="z-20"></app-bar>
+    <nav class="absolute w-64 h-full pt-14 text-center bg-white">
+      <c-button type="primary m-4">Create Quiz</c-button>
+      <ul class="text-left">
+        <c-nav-item>
+          <c-icon>home</c-icon>
+          <span>Home</span>
+        </c-nav-item>
+        <c-nav-item>
+          <c-icon>description</c-icon>
+          <span>My Quiz</span>
+        </c-nav-item>
+        <c-nav-item>
+          <c-icon>info</c-icon>
+          <span>About</span>
+        </c-nav-item>
+      </ul>
+    </nav>
+    <div class="ml-64">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
 import AppBar from "./navigation/CAppBar.vue";
 import Modal from "./modal/Modal";
+import CNavItem from "@/core/components/CNavItem.vue";
 
 export default {
   components: {
     AppBar,
     Modal,
+    CNavItem,
   },
 };
 </script>
