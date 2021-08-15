33d07d9b68b81553b986f6ffbf51375bec2b3821
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-08-15 11:42:47 +0700

    fix(client): CAppBar.stories not refractored

--- client/src/components/CAppBar.stories.js
+++ client/src/components/CAppBar.stories.js
@@ -1,11 +1,11 @@
-import AppBar from "./Navigation.vue";
+import CAppBar from "./CAppBar.vue";
 import router from "@/router";
 
-export default { title: "Navigation" };
+export default { title: "AppBar" };
 
 export const standar = () => ({
   components: {
-    AppBar,
+    CAppBar,
   },
   template: "<c-app-bar></c-app-bar>",
   router,
