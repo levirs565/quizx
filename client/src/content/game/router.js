1b14bac71ea97ca78646dd488c2cccb16b605fdd
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-08-20 20:44:32 +0700

    refractor(client): merge all content routes

--- client/src/content/game/router.js
+++
@@ -1,9 +0,0 @@
-import GameResult from "./views/GameResult.vue";
-
-export default [
-  {
-    path: "permainan/:game_id",
-    component: GameResult,
-    props: true,
-  },
-];
--- client/src/router.js
+++ client/src/router.js
@@ -2,20 +2,11 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import CoreRouter from "@/core/router";
 import ContentRouter from "@/content/router";
-import GameBoard from "@/content/game/views/GameBoard.vue";
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-  routes: [
-    ...CoreRouter,
-    ...ContentRouter,
-    {
-      path: "/permainan/:game_id/board",
-      props: true,
-      component: GameBoard,
-    },
-  ],
+  routes: [...CoreRouter, ...ContentRouter],
 });
 
 export default router;
