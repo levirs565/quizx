c910d3fbf73afe4000840179697158290eb54e61
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-08-20 20:26:40 +0700

    fix:(client) remove font-awesome

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -2,17 +2,5 @@ import { configure } from '@storybook/vue';
 import Vue from 'vue';
 import '@/router';
 import '@/style.css';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import {
-  faCheck,
-  faTimes,
-  faExclamation,
-  faBars
-} from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-
-library.add(faCheck, faTimes, faExclamation, faBars);
-
-Vue.component('font-awesome', FontAwesomeIcon);
 
 configure(require.context('../src', true, /\.stories\.js$/), module);
--- client/src/main.js
+++ client/src/main.js
@@ -2,38 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import {
-  faCheck,
-  faTimes,
-  faExclamation,
-  faBars,
-  faPlay,
-  faUser,
-  faQuestionCircle,
-  faEdit,
-  faTrash,
-} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import vClickOutside from "v-click-outside";
 import "@/style.css";
 import CButton from "@/components/CButton.vue";
 import CIcon from "@/components/CIcon.vue";
 import CIconButton from "@/components/CIconButton.vue";
 
-library.add(
-  faCheck,
-  faTimes,
-  faExclamation,
-  faBars,
-  faPlay,
-  faUser,
-  faQuestionCircle,
-  faEdit,
-  faTrash
-);
-
-Vue.component("font-awesome", FontAwesomeIcon);
 Vue.component("c-button", CButton);
 Vue.component("c-icon", CIcon);
 Vue.component("c-icon-button", CIconButton);
