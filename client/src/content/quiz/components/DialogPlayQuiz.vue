a41da1873f4cc267f3cdadc96b153703465de55e
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-09-18 15:44:25 +0700

    fix(client): migrate c-switch to b-switch

--- client/src/content/quiz/components/DialogPlayQuiz.vue
+++ client/src/content/quiz/components/DialogPlayQuiz.vue
@@ -1,7 +1,9 @@
 <template>
   <c-dialog @submit="$emit('submit', isInteractive)">
     <c-dialog-title>Play Game</c-dialog-title>
-    <c-switch v-model="isInteractive">Interactive Game</c-switch>
+    <b-field>
+      <b-switch v-model="isInteractive">Interactive Game</b-switch>
+    </b-field>
     <c-dialog-buttons>
       <b-button @click.prevent="$emit('hide')">Cancel</b-button>
       <b-button type="is-primary">Play Now</b-button>
@@ -11,10 +13,9 @@
 <script>
 import CDialog from "@/components/dialog/CDialog.vue";
 import CDialogButtons from "@/components/dialog/CDialogButtons.vue";
-import CSwitch from "@/components/CSwitch.vue";
 import CDialogTitle from "@/components/dialog/CDialogTitle.vue";
 export default {
-  components: { CDialog, CDialogButtons, CSwitch, CDialogTitle },
+  components: { CDialog, CDialogButtons, CDialogTitle },
   data() {
     return {
       isInteractive: false,
