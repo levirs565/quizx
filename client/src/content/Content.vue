643eff88a21529270f1efd263e211b5cb1b2c7c1
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-09-19 19:50:09 +0700

    fix(client): use buefy sidebar for navbar
    Use fixed for sidebar. When use sticky, sidebar have some weird behaviour.
    Does not set positon="fixed" parameter (not css) for b-sidebar because it will port sidebar to body.

--- client/src/content/Content.vue
+++ client/src/content/Content.vue
@@ -1,41 +1,73 @@
 <template>
-  <div class="flex flex-col">
+  <div class="page">
     <app-bar></app-bar>
-    <nav class="fixed w-64 h-full top-14 text-center bg-white">
-      <b-button type="is-primary" class="m-4">Create Quiz</b-button>
-      <ul class="text-left">
-        <c-nav-link to="/">
-          <b-icon icon="home" />
-          <span>Home</span>
-        </c-nav-link>
-        <c-nav-link to="/quiz">
-          <b-icon icon="file-document" />
-          <span>My Quiz</span>
-        </c-nav-link>
-        <c-nav-item>
-          <b-icon icon="information" />
-          <span>About</span>
-        </c-nav-item>
-      </ul>
-    </nav>
-    <div class="ml-64 mt-14">
-      <router-view></router-view>
-    </div>
+    <section class="sidebar-layout">
+      <b-sidebar position="static" type="is-light" open fullheight>
+        <div class="p-2 has-text-centered">
+          <b-button type="is-primary my-4">Create Quiz</b-button>
+          <b-menu class="has-text-left" :activable="false" :accordion="false">
+            <b-menu-list>
+              <router-link
+                v-for="(link, index) in links"
+                :key="index"
+                :to="link.to"
+                v-slot="{ href, isExactActive }"
+              >
+                <b-menu-item
+                  :href="href"
+                  :active="isExactActive"
+                  :icon="link.icon"
+                  :label="link.label"
+                />
+              </router-link>
+            </b-menu-list>
+          </b-menu>
+        </div>
+      </b-sidebar>
+    </section>
+    <router-view class="main-layout"></router-view>
   </div>
 </template>
 
 <script>
 import AppBar from "../components/CAppBar.vue";
-import CNavItem from "@/components/CNavItem.vue";
-import CNavLink from "@/components/CNavLink.vue";
 
 export default {
   components: {
     AppBar,
-    CNavItem,
-    CNavLink,
+  },
+  data() {
+    return {
+      links: [
+        {
+          to: "/",
+          icon: "home",
+          label: "Home",
+        },
+        {
+          to: "/quiz",
+          icon: "file-document",
+          label: "Quiz",
+        },
+      ],
+    };
   },
 };
 </script>
 
-<style></style>
+<style>
+/* 56px come from bulma */
+.page .sidebar-layout {
+  position: fixed;
+  top: 56px;
+}
+
+/* 260px is come from bulma */
+.page .main-layout {
+  margin-left: 260px;
+}
+
+.page .sidebar-layout .b-sidebar {
+  height: calc(100vh - 56px);
+}
+</style>
