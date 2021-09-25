3029867422351c123d7017727f9a8d67d71bcdfe
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-09-25 11:36:30 +0700

    fix(server): action handler scheme return nothing

--- server/src/controllers/helper.ts
+++ server/src/controllers/helper.ts
@@ -1,22 +1,26 @@
 import { Response, Request, NextFunction } from 'express';
-import { ajv } from "../validation";
+import { ajv } from '../validation';
 import { ActionSuccessResponse, SchemaDefinition } from '../types/base';
 import { BodyValidationError } from '../error';
+
+function createSuccesResponse() {
+  return {
+    success: true
+  } as ActionSuccessResponse;
+}
 
 export function jsonHandler<T>(fn: (req: Request) => Promise<T>) {
   return (req: Request, res: Response, next: NextFunction) => {
     fn(req)
-      .then((result) => res.json(result))
+      .then(result => res.json(result))
       .catch(next);
   };
 }
 
 export function actionHandler(fn: (req: Request) => Promise<void>) {
-  return jsonHandler(async (req) => {
+  return jsonHandler(async req => {
     await fn(req);
-    return {
-      success: true,
-    } as ActionSuccessResponse;
+    return createSuccesResponse()
   });
 }
 
@@ -25,10 +29,10 @@ export function jsonHandlerSchema<Body, Result>(
   fn: (req: Request<any, any, Body>) => Promise<Result>
 ) {
   const validate = ajv.getSchema(`#/definitions/${bodySchema.name}`)!;
-  return jsonHandler(async (req) => {
+  return jsonHandler(async req => {
     const valid = validate(req.body);
     if (!valid) {
-      throw new BodyValidationError(validate.errors!)
+      throw new BodyValidationError(validate.errors!);
     }
 
     return await fn(req);
@@ -39,5 +43,8 @@ export function actionHandlerSchema<Body>(
   bodySchema: SchemaDefinition<Body>,
   fn: (req: Request<{}, any, Body>) => Promise<void>
 ) {
-  return jsonHandlerSchema(bodySchema, fn)
+  return jsonHandlerSchema(bodySchema, async req => {
+    await fn(req)
+    return createSuccesResponse()
+  });
 }
