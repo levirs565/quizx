05412b074ee1e5b5c5f7ac591eddddc7ee8b54ac
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-12-08 20:56:38 +0700

    fix(client): remove v-click-outside package

--- client/src/main.js
+++ client/src/main.js
@@ -2,13 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import vClickOutside from "v-click-outside";
 import "@/style.css";
 import Buefy from "buefy";
 import "buefy/dist/buefy.css";
 import "@mdi/font/css/materialdesignicons.css";
 
-Vue.use(vClickOutside);
 Vue.use(Buefy);
 
 Vue.config.productionTip = false;
