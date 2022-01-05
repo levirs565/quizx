bb93445fbbcb1aaf772be65d3d0353c580684280
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2022-01-05 17:12:44 +0700

    fix(client): tiptap styling
    Changes:
    - Remove ExtendedTable and use original Table extension
    - Add style from tiptap demo and adapt it with vuetify style

--- client/src/components/TipTapBase.vue
+++ client/src/components/TipTapBase.vue
@@ -1,7 +1,7 @@
 <script>
 import { Editor, EditorContent } from "@tiptap/vue-2";
 import StarterKit from "@tiptap/starter-kit";
-import ExtendedTable from "./extensions/ExtendedTable";
+import Table from "@tiptap/extension-table";
 import TableRow from "@tiptap/extension-table-row";
 import TableCell from "@tiptap/extension-table-cell";
 import TableHeader from "@tiptap/extension-table-header";
@@ -37,7 +37,7 @@ export default {
       content: this.value,
       extensions: [
         StarterKit,
-        ExtendedTable,
+        Table,
         TableRow,
         TableHeader,
         TableCell,
@@ -67,3 +67,98 @@ export default {
   },
 };
 </script>
+<style lang="scss">
+/* Basic editor styles */
+.ProseMirror {
+  margin: 1rem 0;
+  --divider: rgba(0, 0, 0, 0.12);
+  --code-color: rgba(0, 0, 0, 0.05);
+  --active-color: #f5f5f5;
+
+  > * + * {
+    margin-top: 0.75em;
+  }
+
+  ul,
+  ol {
+    padding: 0 1rem;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    line-height: 1.1;
+  }
+
+  pre {
+    background: var(--code-color);
+    padding: 0.75rem 1rem;
+
+    code {
+      color: inherit;
+      padding: 0;
+      background-color: transparent !important;
+    }
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+
+  blockquote {
+    padding-left: 1rem;
+    border-left: 2px solid var(--divider);
+  }
+
+  hr {
+    border: none;
+    border-top: 2px solid var(--divider);
+    margin: 2rem 0;
+  }
+}
+
+/* Table-specific styling */
+.ProseMirror {
+  table {
+    border-collapse: collapse;
+    table-layout: fixed;
+    width: 100%;
+    margin: 0;
+    overflow: hidden;
+
+    td,
+    th {
+      min-width: 1em;
+      border: thin solid var(--divider);
+      padding: 3px 5px;
+      vertical-align: top;
+      box-sizing: border-box;
+      position: relative;
+
+      > * {
+        margin-bottom: 0;
+      }
+    }
+
+    th {
+      font-weight: bold;
+    }
+
+    .selectedCell {
+      background: var(--active-color);
+    }
+
+    p {
+      margin: 0;
+    }
+  }
+}
+
+.tableWrapper {
+  overflow-x: auto;
+}
+</style>
