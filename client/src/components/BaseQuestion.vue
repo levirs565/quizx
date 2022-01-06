2fb9d34a95726bd00dfbdb9110dd639fa8af2a1d
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2022-01-06 17:43:51 +0700

    perf(client): make question component lazy
    Question component is very heavy component.

--- client/src/components/BaseQuestion.vue
+++ client/src/components/BaseQuestion.vue
@@ -1,74 +1,78 @@
 <template>
-  <v-card>
-    <v-card-title class="text-overline">Question {{ index + 1 }}</v-card-title>
-    <v-card-text>
-      <slot name="question" />
-
-      <v-radio-group
-        v-if="question.type === 'multiple-choice'"
-        v-model="lazyAnswer"
-        :readonly="!answerEditable"
-        v-bind="inputProps"
+  <v-lazy min-height="200px">
+    <v-card>
+      <v-card-title class="text-overline"
+        >Question {{ index + 1 }}</v-card-title
       >
-        <v-row
-          v-for="(entry, choiceIndex) in question.choices"
-          :key="choiceIndex"
-          no-gutters
-          class="question-choice"
+      <v-card-text>
+        <slot name="question" />
+
+        <v-radio-group
+          v-if="question.type === 'multiple-choice'"
+          v-model="lazyAnswer"
+          :readonly="!answerEditable"
+          v-bind="inputProps"
         >
-          <v-radio :value="choiceIndex" class="question-radio">
-            <template v-slot:label>
-              <slot
-                name="choice"
-                v-bind="{
-                  choice: entry,
-                  index: choiceIndex,
-                  indexChar: getChoiceIndex(choiceIndex),
-                }"
-              />
-            </template>
-          </v-radio>
+          <v-row
+            v-for="(entry, choiceIndex) in question.choices"
+            :key="choiceIndex"
+            no-gutters
+            class="question-choice"
+          >
+            <v-radio :value="choiceIndex" class="question-radio">
+              <template v-slot:label>
+                <slot
+                  name="choice"
+                  v-bind="{
+                    choice: entry,
+                    index: choiceIndex,
+                    indexChar: getChoiceIndex(choiceIndex),
+                  }"
+                />
+              </template>
+            </v-radio>
 
-          <slot
-            name="afterChoice"
-            v-bind="{
-              choice: entry,
-              index: choiceIndex,
-              indexChar: getChoiceIndex(choiceIndex),
-            }"
-          />
-        </v-row>
-      </v-radio-group>
-      <v-text-field
-        v-else-if="question.type == 'short-text'"
-        v-model="lazyAnswer"
-        type="text"
-        outlined
-        :readonly="!answerEditable"
-        v-bind="inputProps"
-      />
-      <v-text-field
-        v-else-if="question.type == 'number'"
-        type="number"
-        v-model.number="lazyAnswer"
-        outlined
-        :readonly="!answerEditable"
-        v-bind="inputProps"
-      />
-      <math-field-input
-        v-else-if="question.type == 'math'"
-        outlined
-        v-model="lazyAnswer"
-        virtual-keyboard-mode="manual"
-        :readonly="!answerEditable"
-        v-bind="inputProps"
-      />
+            <slot
+              name="afterChoice"
+              v-bind="{
+                choice: entry,
+                index: choiceIndex,
+                indexChar: getChoiceIndex(choiceIndex),
+              }"
+            />
+          </v-row>
+        </v-radio-group>
+        <v-text-field
+          v-else-if="question.type == 'short-text'"
+          v-model="lazyAnswer"
+          type="text"
+          outlined
+          :readonly="!answerEditable"
+          v-bind="inputProps"
+        />
+        <v-text-field
+          v-else-if="question.type == 'number'"
+          type="number"
+          v-model.number="lazyAnswer"
+          outlined
+          :readonly="!answerEditable"
+          v-bind="inputProps"
+        />
+        <math-field-input
+          v-else-if="question.type == 'math'"
+          outlined
+          v-model="lazyAnswer"
+          virtual-keyboard-mode="manual"
+          :readonly="!answerEditable"
+          v-bind="inputProps"
+        />
 
-      <slot name="content" />
-    </v-card-text>
+        <slot name="content" />
+      </v-card-text>
 
-    <slot v-bind:component="this"></slot>
-  </v-card>
+      <slot v-bind:component="this"></slot>
+    </v-card>
+  </v-lazy>
 </template>
 
 <script>
