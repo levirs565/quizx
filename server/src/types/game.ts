6da7fed1f2e877c4bad6885fe8824f9ea05015dc
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2022-01-28 14:02:57 +0700

    multi game mode in types

--- server/src/types/game.ts
+++ server/src/types/game.ts
@@ -1,18 +1,52 @@
 import { AutoMap } from '@automapper/classes';
 import { Type } from 'class-transformer';
-import { IsBoolean, IsString } from 'class-validator';
+import { IsBoolean, IsNumber, IsPositive, IsString, Min } from 'class-validator';
 import { Question, QuestionAnswer, questionDiscriminator } from './quiz';
 
-export class GamePreference {
-  @IsBoolean()
-  isInteractive!: boolean;
+export abstract class GamePreference {
   @IsBoolean()
   shuffleQuestions!: boolean;
 }
 
-export class PlayGameRequestBody extends GamePreference {
+export class FlashCardGamePreference extends GamePreference {
+  @IsNumber()
+  @Min(0)
+  questionTimeMinute?: Number;
+  @IsNumber()
+  @Min(0)
+  retryCount?: Number;
+}
+
+export class ExamGamePreference extends GamePreference {
+  @IsNumber()
+  @Min(0)
+  examTimeMinute?: Number;
+}
+
+enum GameType {
+  Exam = 'exam',
+  FlashCard = 'flash-card'
+}
+
+export class PlayGameRequestBody {
   @IsString()
   quizId!: string;
+  @Type(() => GamePreference, {
+    discriminator: {
+      property: 'type',
+      subTypes: [
+        {
+          name: GameType.FlashCard,
+          value: FlashCardGamePreference,
+        },
+        {
+          name: GameType.Exam,
+          value: ExamGamePreference,
+        },
+      ],
+    },
+  })
+  preference!: GamePreference;
 }
 
 export enum QuestionState {
@@ -25,10 +59,9 @@ export interface GameResult {
   unanswered: number;
   correct: number;
   wrong: number;
-  questionsState: Array<QuestionState>;
 }
 
-export class GameSummary extends GamePreference {
+export class GameSummary {
   @AutoMap()
   id!: string;
   @AutoMap()
@@ -41,12 +74,49 @@ export class GameSummary extends GamePreference {
   isPlaying!: boolean;
   @AutoMap()
   result?: GameResult;
+  @AutoMap()
+  finishTime!: Date;
+  @AutoMap()
+  endTime!: Date;
 }
 
-export class Game extends GameSummary {
+export abstract class GameData {
+  peference!: GamePreference;
+}
+
+export class FlashCardGameData extends GameData {
+  preference!: FlashCardGamePreference;
+  currentQuestionIndex!: number;
+  currentQuestionMaxTime!: number;
+}
+
+export class ExamGameData extends GameData {
+  peference!: ExamGamePreference;
+  maxFinishTime!: Date;
+}
+
+export abstract class Game extends GameSummary {
   @Type(() => Question, {
     discriminator: questionDiscriminator,
   })
   questions!: Array<Question>;
+  questionsState!: Array<QuestionState>;
   correctAnswers?: Array<QuestionAnswer>;
+  @Type(() => GameData, {
+    discriminator: {
+      property: 'type',
+      subTypes: [
+        {
+          name: GameType.Exam,
+          value: ExamGameData
+        },
+        {
+          name: GameType.FlashCard,
+          value: FlashCardGameData
+        }
+      ]
+    }
+  })
+  data!: GameData;
 }
+
